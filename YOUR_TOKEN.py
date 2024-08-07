debd0a68e00cbc78ae7c316ba438ed89159f8652
prokent <pro.kent676@gmail.com>
2024-08-07 08:36:09 +0200

    make it better

--- YOUR_TOKEN.py
+++ YOUR_TOKEN.py
@@ -1,2 +1,2 @@
 YOUR_BOT_TOKEN = '7112295260:AAFpQ1Cqo31Odq-69t54stivkoJ21eTJkug'
-FLASK_SERVER_URL = 'https://kent12-64aba4c14b55.herokuapp.com/get-messages'
+FLASK_SERVER_URL = 'https://kent12-64aba4c14b55.herokuapp.com/send-message'
--- flask_server.py
+++ flask_server.py
@@ -18,7 +18,7 @@ def receive_message():
         chat_id = data.get('chatid')
 
         if chat_id and text:
-            bot.send_message(chat_id, f"Дурак пишетттт: {text}")
+            bot.send_message(chat_id, f"Дурак пишет: {text}")
             return jsonify({"status": "success"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data provided"}), 400
@@ -34,6 +34,8 @@ def save_message():
         text = data.get('message')
         chat_id = data.get('chatid')
 
+        print(f"Получены данные: {data}")
+
         if username and text and chat_id:
             # Сохраняем данные в словарь
             stored_messages.append({
@@ -41,6 +43,7 @@ def save_message():
                 'message': text,
                 'chatid': chat_id
             })
+            print(f"Сообщение сохранено: {stored_messages[-1]}")
             return jsonify({"status": "success", "message": "Message saved"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data provided"}), 400
@@ -48,10 +51,9 @@ def save_message():
         print(f"Ошибка при сохранении данных: {e}")
         return jsonify({"status": "error", "message": "Internal server error"}), 500
 
-# Опционально, страница для получения всех сохраненных сообщений
 @app.route('/get-messages', methods=['GET'])
 def get_messages():
     return jsonify(stored_messages), 200
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
