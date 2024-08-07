08f42d91be4bec0b8fbf0da3461f2588ec2a0f7c
prokent <pro.kent676@gmail.com>
2024-08-07 08:49:46 +0200

    make it better

--- flask_server.py
+++ flask_server.py
@@ -1,16 +1,12 @@
 from flask import Flask, request, jsonify
 import telebot
-import asyncio
 from YOUR_TOKEN import YOUR_BOT_TOKEN
-
 bot = telebot.TeleBot(YOUR_BOT_TOKEN)
+
 app = Flask(__name__)
 
-# Создаем список для хранения сообщений
-stored_messages = []
-
 @app.route('/send-message', methods=['POST'])
-async def receive_message():
+def receive_message():
     try:
         data = request.get_json()
         username = data.get('username')
@@ -18,26 +14,13 @@ async def receive_message():
         chat_id = data.get('chatid')
 
         if chat_id and text:
-            # Отправка сообщения с использованием asyncio для асинхронного выполнения
-            await asyncio.to_thread(bot.send_message, chat_id, f"Дурак пишет: {text}")
-
-            # Сохранение сообщения
-            stored_messages.append({
-                'username': username,
-                'message': text,
-                'chatid': chat_id
-            })
-            return jsonify({"status": "success", "message": "Message sent"}), 200
+            bot.send_message(chat_id, f"Дурак пишет: {text}")
+            return jsonify({"status": "success"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data provided"}), 400
     except Exception as e:
         print(f"Ошибка при получении данных: {e}")
         return jsonify({"status": "error", "message": "Internal server error"}), 500
 
-# Эндпоинт для получения сохраненных сообщений
-@app.route('/get-messages', methods=['GET'])
-def get_messages():
-    return jsonify(stored_messages), 200
-
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
