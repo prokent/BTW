0f95c781ecc9deb51a18bc369089fcb3ea1be9ea
prokent <pro.kent676@gmail.com>
2024-08-07 08:32:22 +0200

    make it better

--- flask_server.py
+++ flask_server.py
@@ -18,7 +18,7 @@ def receive_message():
         chat_id = data.get('chatid')
 
         if chat_id and text:
-            bot.send_message(chat_id, f"Дурак пишет: {text}")
+            bot.send_message(chat_id, f"Дурак пишетттт: {text}")
             return jsonify({"status": "success"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data provided"}), 400
