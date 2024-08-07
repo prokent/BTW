9cc029c6a8bceda0d2dc421850b177d1f7dd397a
prokent <pro.kent676@gmail.com>
2024-08-07 10:33:30 +0200

    make it better

--- YOUR_TOKEN.py
+++ YOUR_TOKEN.py
@@ -1,2 +1,2 @@
 YOUR_BOT_TOKEN = '7112295260:AAFpQ1Cqo31Odq-69t54stivkoJ21eTJkug'
-FLASK_SERVER_URL = 'https://echo-btwgroup-89ab7dbe3498.herokuapp.com/send-message'
+FLASK_SERVER_URL = 'https://kent12-64aba4c14b55.herokuapp.com/send-message'
--- flask_server.py
+++ flask_server.py
@@ -12,27 +12,23 @@ app = Flask(__name__)
 # Включаем CORS для всего приложения
 CORS(app)
 
-@app.route('/send-message', methods=['GET'])
+
+@app.route('/send-message', methods=['POST'])
 def receive_message():
     try:
-        # Получаем данные из строки запроса (query parameters)
-        username = request.args.get('username')
-        text = request.args.get('message')
-        chat_id = request.args.get('chatid')
+        data = request.get_json()
+        username = data.get('username')
+        text = data.get('message')
+        chat_id = data.get('chatid')
 
-        # Проверяем наличие chat_id и текста сообщения
         if chat_id and text:
             bot.send_message(chat_id, f"Дурак пишет: {text}")
             return jsonify({"status": "success"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data provided"}), 400
-
     except Exception as e:
-        # Логируем ошибку и возвращаем статус 500
         print(f"Ошибка при получении данных: {e}")
         return jsonify({"status": "error", "message": "Internal server error"}), 500
 
-
-# Запускаем приложение
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
