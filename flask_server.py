c203e850c080b6d3e4750f757278c2f8dded5e99
prokent <pro.kent676@gmail.com>
2024-08-07 10:01:43 +0200

    make it better

--- flask_server.py
+++ flask_server.py
@@ -1,12 +1,9 @@
 from flask import Flask, request, jsonify
 import telebot
 from YOUR_TOKEN import YOUR_BOT_TOKEN
-
 bot = telebot.TeleBot(YOUR_BOT_TOKEN)
-app = Flask(__name__)
 
-# Список для хранения сообщений
-stored_messages = []
+app = Flask(__name__)
 
 @app.route('/send-message', methods=['POST'])
 def receive_message():
@@ -17,39 +14,13 @@ def receive_message():
         chat_id = data.get('chatid')
 
         if chat_id and text:
-            bot.send_message(chat_id, f"Турок Успешно отправил на сервер: {text}")
+            bot.send_message(chat_id, f"Дурак пишет: {text}")
             return jsonify({"status": "success"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data provided"}), 400
     except Exception as e:
-        print(f"Error receiving data: {e}")
+        print(f"Ошибка при получении данных: {e}")
         return jsonify({"status": "error", "message": "Internal server error"}), 500
-
-@app.route('/save-message', methods=['POST'])
-def save_message():
-    try:
-        data = request.get_json()
-        username = data.get('username')
-        text = data.get('message')
-        chat_id = data.get('chatid')
-
-        if username and text and chat_id:
-            stored_messages.append({
-                'username': username,
-                'message': text,
-                'chatid': chat_id
-            })
-            print(f"Message saved: {stored_messages[-1]}")
-            return jsonify({"status": "success", "message": "Message saved"}), 200
-        else:
-            return jsonify({"status": "error", "message": "Invalid data provided"}), 400
-    except Exception as e:
-        print(f"Error saving data: {e}")
-        return jsonify({"status": "error", "message": "Internal server error"}), 500
-
-@app.route('/get-messages', methods=['GET'])
-def get_messages():
-    return jsonify(stored_messages), 200
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
