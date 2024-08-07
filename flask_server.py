9bb91fd93ecf74f58a7ba40421c3f7586ff895de
prokent <pro.kent676@gmail.com>
2024-08-07 10:38:46 +0200

    make it better

--- flask_server.py
+++ flask_server.py
@@ -3,15 +3,9 @@ from flask_cors import CORS
 import telebot
 from YOUR_TOKEN import YOUR_BOT_TOKEN
 
-# Создаем экземпляр бота
 bot = telebot.TeleBot(YOUR_BOT_TOKEN)
-
-# Создаем экземпляр Flask приложения
 app = Flask(__name__)
-
-# Включаем CORS для всего приложения
 CORS(app)
-
 
 @app.route('/send-message', methods=['POST'])
 def receive_message():
@@ -22,12 +16,12 @@ def receive_message():
         chat_id = data.get('chatid')
 
         if chat_id and text:
-            bot.send_message(chat_id, f"Дурак пишет: {text}")
+            bot.send_message(chat_id,  {text})
             return jsonify({"status": "success"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data provided"}), 400
     except Exception as e:
-        print(f"Ошибка при получении данных: {e}")
+        print(f"Ошибка при получении: {e}")
         return jsonify({"status": "error", "message": "Internal server error"}), 500
 
 if __name__ == '__main__':
