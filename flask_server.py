5a910b8f0d422faa1301f2f9450f323bd9552ace
prokent <pro.kent676@gmail.com>
2024-08-07 07:08:54 +0200

    make it better

--- flask_server.py
+++ flask_server.py
@@ -1,11 +1,29 @@
 from flask import Flask, request, jsonify
 import telebot
+import sqlite3
 import os
 
-YOUR_BOT_TOKEN = '7112295260:AAFpQ1Cqo31Odq-69t54stivkoJ21eTJkug'
-bot = telebot.TeleBot(YOUR_BOT_TOKEN)
+YOUR_NEW_BOT_TOKEN = '7112295260:AAFpQ1Cqo31Odq-69t54stivkoJ21eTJkug'
+bot = telebot.TeleBot(YOUR_NEW_BOT_TOKEN)
 
 app = Flask(__name__)
+
+# Функция для создания таблицы сообщений, если она не существует
+def create_table():
+    with sqlite3.connect('messages.db') as conn:
+        c = conn.cursor()
+        c.execute('''
+            CREATE TABLE IF NOT EXISTS messages (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                username TEXT,
+                message TEXT,
+                chat_id TEXT
+            )
+        ''')
+        conn.commit()
+
+# Вызов функции создания таблицы при запуске приложения
+create_table()
 
 @app.route('/send-message', methods=['POST'])
 def receive_message():
@@ -16,7 +34,17 @@ def receive_message():
         chat_id = data.get('chatid')
 
         if chat_id and text:
-            bot.send_message(chat_id, f"Дурак пишет: {text}")
+            # Сохранение сообщения в базе данных
+            with sqlite3.connect('messages.db') as conn:
+                c = conn.cursor()
+                c.execute('''
+                    INSERT INTO messages (username, message, chat_id)
+                    VALUES (?, ?, ?)
+                ''', (username, text, chat_id))
+                conn.commit()
+
+            # Отправка сообщения через бота
+            bot.send_message(chat_id, f"Сообщение от сервера: {text}")
             return jsonify({"status": "success"}), 200
         else:
             return jsonify({"status": "error", "message": "Invalid data provided"}), 400
